e2cbfc827097b989fbe260ddf3fc5a555e8bad10
Natalia <kashitsyna.2001@mail.ru>
2024-11-02 11:14:48 +0400

    Добавила Vuetify, Pinia, Typescript, sass, eslint

--- AP3-store/nuxt.config.ts
+++ AP3-store/nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['vuetify-nuxt-module', '@pinia/nuxt', '@nuxt/eslint'],
+  vuetify: {
+    moduleOptions: {
+      /* module specific options */
+    },
+    vuetifyOptions: {
+      /* vuetify options */
+    }
+  }
 })
