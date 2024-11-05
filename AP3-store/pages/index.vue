c0e61130559d07ec5d239f0c2191ae96ef9a9a5e
Natalia <kashitsyna.2001@mail.ru>
2024-11-05 13:00:47 +0400

    fix it

--- AP3-store/pages/index.vue
+++ AP3-store/pages/index.vue
@@ -1,6 +1,18 @@
 <template>
-    <div>
+<div class="catalog__wrapper">
         <Filter :filter-data="brands"/>
+
+        <div class="catalog__items__wrapper">
+            <p class="catalog__title">Catalog</p>
+            <div class="catalog__items">
+                <div
+                    v-for="(product, productId) in products" 
+                    :key="productId" 
+                    class="catalog__item">
+                    <Product :product-data="product"/>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -18,17 +30,17 @@
         brandName?: string;
     }
 
-    let brands = useState<Brand>('setBrands', () => []);
+    let brands = useState<Brand>('setBrands', () => []); // в пиниа
     let products = useState<Product>('setProducts', () => []);
 
     const fetchBrands = async () => {
         try {
-            brands = await $fetch<Brand[]>('/brands.json');
-            if (brands[0].id !== 0) {
-                brands = [{ 
+            brands.value = await $fetch<Brand[]>('/brands.json');
+            if (brands.value[0].id !== 0) {
+                brands.value = [{ 
                     id: 0,
                     title: "All Brands"
-                }, ...brands];
+                }, ...brands.value];
             }
         } catch (error) {
             console.error(error);
@@ -37,8 +49,8 @@
 
     const fetchProducts = async () => {
         try {
-            products = await $fetch<Product[]>('/products.json');
-            products.forEach((product: Product) => {
+            products.value = await $fetch<Product[]>('/products.json');
+            products.value.forEach((product: Product) => {
                 product.quantity = 1;
                 const brand = brands.value.find((brand: Brand) => product.brand === brand.id);
                 if (brand) product.brandName = brand.title;
@@ -54,3 +66,40 @@
         fetchProducts();
     });
 </script>
+
+<style scoped lang='scss'>
+    .catalog__wrapper {
+        display: flex;
+        flex-direction: row;
+        gap: 100px;
+        padding: 20px 5%;
+    }
+    .catalog__filters {
+        flex: 1 1 15%;
+    }
+    .catalog__items__wrapper {
+        flex: 1 1 85%;
+        display: block;
+    }
+    .catalog__items {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: flex-start;
+        gap: 50px;
+    }
+    .catalog__title {
+        font-size: 24px;
+        margin: 0;
+        padding: 5px;
+    }
+    @media(max-width: 768px) {
+        .catalog__wrapper {
+            flex-direction: column;
+            gap: 20px;
+        }
+        .catalog__items {
+            justify-content: center;
+        }
+    }
+</style>
