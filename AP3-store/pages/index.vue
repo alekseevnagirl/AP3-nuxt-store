774d4f8157e485261a8c8f061259ad349677df10
Natalia <kashitsyna.2001@mail.ru>
2024-11-05 11:12:44 +0400

    add json data

--- AP3-store/pages/index.vue
+++ AP3-store/pages/index.vue
@@ -1,30 +1,56 @@
 <template>
     <div>
-        <Filter :filter-data="filterData"/>
+        <Filter :filter-data="brands"/>
     </div>
 </template>
 
 <script setup lang="ts">
-    const filterData = ref(
-        [
-            {
-                "id": 1,
-                "title": "Brand 1",
-                "sort": "100",
-                "code": "brand_1"
-            },
-            {
-                "id": 2,
-                "title": "Brand 2",
-                "sort": "200",
-                "code": "brand_2"
-            },
-            {
-                "id": 3,
-                "title": "Brand 3",
-                "sort": "300",
-                "code": "brand_3"
+    interface Brand {
+        id: number;
+        title: string;
+    }
+
+    interface Product {
+        id: number;
+        title: string;
+        brand: number;
+        quantity?: number;
+        brandName?: string;
+    }
+
+    let brands = useState<Brand>('setBrands', () => []);
+    let products = useState<Product>('setProducts', () => []);
+
+    const fetchBrands = async () => {
+        try {
+            brands = await $fetch<Brand[]>('/brands.json');
+            if (brands[0].id !== 0) {
+                brands = [{ 
+                    id: 0,
+                    title: "All Brands"
+                }, ...brands];
             }
-        ]
-    )
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const fetchProducts = async () => {
+        try {
+            products = await $fetch<Product[]>('/products.json');
+            products.forEach((product: Product) => {
+                product.quantity = 1;
+                const brand = brands.value.find((brand: Brand) => product.brand === brand.id);
+                if (brand) product.brandName = brand.title;
+                else product.brandName = '';
+            })
+        } catch (error) {
+        console.error(error);
+        }
+    };
+
+    onMounted(() => {
+        fetchBrands();
+        fetchProducts();
+    });
 </script>
