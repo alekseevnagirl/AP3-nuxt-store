ebbc80589fbf9c83446a8055669e8ea688cf7656
Natalia <kashitsyna.2001@mail.ru>
2024-11-07 12:45:26 +0400

    Фикс типов

--- AP3-store/types.ts
+++ AP3-store/types.ts
@@ -1,28 +1,36 @@
 export interface Brand {
     id: number;
     title: string;
+    sort: string;
+    code: string;
 }
 
 export interface Product {
+    type: string;
     id: number;
+    sku: string;
     title: string;
-    brand: number;
-    quantity: number;
-    brandName: string;
     regular_price: {
         currency: string;
         value: number;
-    }
+    };
+    image: string;
+    brand: number;
+    quantity: number;
+    brandName: string;
 }
 
 export interface Cart {
+    type: string;
     id: number;
+    sku: string;
     title: string;
-    brand: number;
-    quantity: number;
-    brandName: string;
     regular_price: {
         currency: string;
         value: number;
-    }
+    };
+    image: string;
+    brand: number;
+    quantity: number;
+    brandName: string;
 }
