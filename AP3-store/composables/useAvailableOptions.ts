ac47bc50ae13f81d1935b1a19016610ac6cdacc0
Natalia <kashitsyna.2001@mail.ru>
2024-11-08 16:55:57 +0400

    Фикс

---
+++ AP3-store/composables/useAvailableOptions.ts
@@ -0,0 +1,7 @@
+import type { Variant } from '~/types'
+
+export function useAvailableOptions(availableVariants: Variant[], variants: Variant[], optionId: number) {
+    let availableOptions = [];
+
+    return availableOptions
+}
