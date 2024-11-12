3a3ca5c444dd1dd30b225f94cd4fba42d5c18f83
Natalia <kashitsyna.2001@mail.ru>
2024-11-12 12:32:27 +0400

    Разделила компонент продукт на подкомпоненты

--- AP3-store/components/Product.vue
+++ AP3-store/components/Product.vue
@@ -1,48 +1,17 @@
 <template>
     <div 
         class="product__wrapper">
-        <v-card>
-            <v-img
-                :width="250"
-                cover
-                :src="productData.image"
-            />
-
-            <v-card-title>
-                {{ productData.title }}
-            </v-card-title>
-
-            <v-card-subtitle class="product__subtitle">
-                {{ subtitle }}
-            </v-card-subtitle>
-
-            <v-card-actions>
-                <v-btn 
-                    class="product__button"
-                    @click="addProduct">
-                    Добавить
-                </v-btn>
-            </v-card-actions>
-        </v-card>
+        <SimpleProduct 
+            v-if="productData.type === 'simple'"
+            :product-data="productData"/>
+        
+        <ConfigurableProduct v-else/>
     </div>
 </template>
 
 <script setup lang="ts">
-    import deepClone from 'lodash.clonedeep';
     import type { Product } from '~/types'
-
     const props = defineProps<{ productData: Product}>();
-
-    const subtitle = computed(() => {
-        const price = getPrice(props.productData?.regular_price?.value, props.productData?.regular_price?.currency);
-        return `${props.productData.brandName}\n${price}`;
-    })
-    
-    const addProduct = () => {
-        const currentVariant = deepClone(props.productData);
-        const cartStore = useCartStore()
-        cartStore.addToCart(currentVariant);
-    }
 </script>
 
 <style scoped lang="scss">
@@ -51,11 +20,5 @@
         gap: 5px;
         padding: 5px;
         border: 1px solid #fff;
-    }
-    .product__subtitle {
-        white-space: pre-line;
-    }
-    .product__button {
-        width: 100%;
     }
 </style>
---
+++ AP3-store/components/SimpleProduct.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-card>
+        <v-img
+            :width="250"
+            cover
+            :src="productData.image"
+        />
+
+        <v-card-title>
+            {{ productData.title }}
+        </v-card-title>
+
+        <v-card-subtitle class="product__subtitle">
+            {{ subtitle }}
+        </v-card-subtitle>
+
+        <slot/>
+
+        <v-card-actions>
+            <v-btn 
+                class="product__button"
+                @click="addProduct">
+                Добавить
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+    import deepClone from 'lodash.clonedeep';
+    import type { Product } from '~/types'
+
+    const props = defineProps<{ productData: Product}>();
+
+    const subtitle = computed(() => {
+        const price = getPrice(props.productData?.regular_price?.value, props.productData?.regular_price?.currency);
+        return `${props.productData.brandName}\n${price}`;
+    })
+    
+    const addProduct = () => {
+        const currentVariant = deepClone(props.productData);
+        const cartStore = useCartStore()
+        cartStore.addToCart(currentVariant);
+    }
+</script>
+
+<style scoped lang="scss">
+    .product__subtitle {
+        white-space: pre-line;
+    }
+    .product__button {
+        width: 100%;
+    }
+</style>
